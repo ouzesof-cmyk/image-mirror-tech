b8801cf297b0cd927a45e2355aec498efb3f7806
ouzesof-cmyk <ouzesof@gmail.com>
2026-05-28 20:46:32 +0100

    Delete supabase directory

--- supabase/migrations/20260510002215_dc205ddf-f7f4-4923-b35a-6ccfac4441b9.sql
+++
@@ -1,8 +0,0 @@
-
-REVOKE EXECUTE ON FUNCTION public.has_role(uuid, app_role) FROM PUBLIC, anon;
-GRANT EXECUTE ON FUNCTION public.has_role(uuid, app_role) TO authenticated;
-
--- Replace broad listing policy
-DROP POLICY IF EXISTS "Public read media" ON storage.objects;
-CREATE POLICY "Public read media objects" ON storage.objects
-  FOR SELECT USING (bucket_id = 'media');
